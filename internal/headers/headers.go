3bb1013cd643c129b2c11bc12d8f06357269cfe7
mrbaker1917 <mrbaker1917@gmail.com>
2026-04-11 08:12:42 -0700

    adds tests and code for character checking and adding values to same key in header map

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -23,15 +23,40 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 		return 2, true, nil
 	}
 	headerLineText := string(data[:idx])
+	n = len(headerLineText) + 2
 	key, val, found := strings.Cut(headerLineText, ":")
 	if !found {
 		return 0, false, errors.New("data is not in key-value format")
 	}
-	keyTrim := strings.TrimSpace(key)
+	keyTrim := strings.TrimRight(key, " ")
 	if len(keyTrim) != len(key) {
 		return 0, false, errors.New("No spaces allowed in the field name/key")
 	}
+	if !isValidHeaderKey(keyTrim) {
+		return 0, false, errors.New("invalid characters in header key")
+	}
+
 	val = strings.TrimSpace(val)
-	h[key] = val
-	return idx + 2, false, nil
+	key = strings.TrimSpace(key)
+	key = strings.ToLower(key)
+
+	if _, ok := h[key]; ok {
+		preval := h[key]
+		h[key] = preval + ", " + val
+		return n, false, nil
+	} else {
+		h[key] = val
+	}
+
+	return n, false, nil
+}
+
+func isValidHeaderKey(key string) bool {
+	allowed := " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'*+-.^_`|~"
+	for _, c := range key {
+		if !strings.ContainsRune(allowed, c) {
+			return false
+		}
+	}
+	return true
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -7,16 +7,47 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestHeaders(t *testing.T) {
+func TestHeadersParse(t *testing.T) {
 	// Test: Valid single header
 	headers := NewHeaders()
 	data := []byte("Host: localhost:42069\r\n\r\n")
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
+
+	// Test: Valid single header with extra whitespace
+	headers = NewHeaders()
+	data = []byte("       Host: localhost:42069                           \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 57, n)
+	assert.False(t, done)
+
+	// Test: Valid 2 headers with existing headers
+	headers = map[string]string{"host": "localhost:42069"}
+	data = []byte("User-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, "curl/7.81.0", headers["user-agent"])
+	assert.Equal(t, 25, n)
+	assert.False(t, done)
+
+	// Test: Valid done
+	headers = NewHeaders()
+	data = []byte("\r\n a bunch of other stuff")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Empty(t, headers)
+	assert.Equal(t, 2, n)
+	assert.True(t, done)
 
 	// Test: Invalid spacing header
 	headers = NewHeaders()
@@ -26,30 +57,30 @@ func TestHeaders(t *testing.T) {
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
 
-	// Test: Valid single header with extra whitespace
+	// Test: Invalid character header
 	headers = NewHeaders()
-	data = []byte("Host:      localhost:42069      \r\n\r\n")
+	data = []byte("H©st: localhost:42069\r\n\r\n")
 	n, done, err = headers.Parse(data)
-	require.NoError(t, err)
-	assert.Equal(t, 34, n)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
 	assert.False(t, done)
 
-	// Test: Valid 2 headers with existing headers:
-	headers = Headers{"Host": "localhost:42069"}
-	data = []byte("User-Agent: curl/7.81.0\r\n\r\n")
+	headers = map[string]string{"host": "localhost:42069"}
+	data = []byte("       Host: localhost:42070       \r\n\r\n")
 	n, done, err = headers.Parse(data)
 	require.NoError(t, err)
-	assert.Equal(t, 25, n)
-	assert.Equal(t, "curl/7.81.0", headers["User-Agent"])
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069, localhost:42070", headers["host"])
+	assert.Equal(t, 37, n)
 	assert.False(t, done)
 
-	// Test: Valid done
-	headers = NewHeaders()
-	data = []byte("\r\n")
+	// Test: Same header key
+	headers = map[string]string{"host": "localhost:8000"}
+	data = []byte("Host: localhost:42069\r\n\r\n")
 	n, done, err = headers.Parse(data)
 	require.NoError(t, err)
-	assert.Equal(t, 2, n)
-	assert.Equal(t, true, done)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:8000, localhost:42069", headers["host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
 }
